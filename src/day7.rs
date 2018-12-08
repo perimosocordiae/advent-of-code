1b2dcd81932a1f05e7cd06f143975c033247a4ee
CJ Carey <perimosocordiae@gmail.com>
2018-12-08 12:51:05 -0500

    Finishing day7 part 2

--- src/day7.rs
+++ src/day7.rs
@@ -36,10 +36,7 @@ fn part1(graph: &DiGraphMap<char, ()>) -> String {
     let mut order = Vec::new();
     while !nodes.is_empty() {
         for i in nodes.into_iter() {
-            if g.neighbors_directed(i, Direction::Incoming)
-                .next()
-                .is_none()
-            {
+            if has_no_incoming(&g, i) {
                 order.push(i);
                 g.remove_node(i);
                 break;
@@ -51,12 +48,58 @@ fn part1(graph: &DiGraphMap<char, ()>) -> String {
     order.into_iter().collect()
 }
 
-fn part2(data: &DiGraphMap<char, ()>) -> i32 {
+fn part2(graph: &DiGraphMap<char, ()>) -> i32 {
     let mut elapsed: i32 = 0;
-    elapsed
+    let mut g = graph.clone();
+    let mut workers = [Worker { node: '0', time: 0 }; 5];
+    while g.node_count() > 0 {
+        let ready: Vec<usize> = workers
+            .iter()
+            .enumerate()
+            .filter(|(_, w)| w.time <= elapsed)
+            .map(|(idx, _)| idx)
+            .collect();
+        let mut did_work = false;
+        for idx in ready {
+            if workers[idx].node != '0' {
+                g.remove_node(workers[idx].node);
+            }
+            let mut nodes: Vec<char> = g.node_identifiers().collect();
+            nodes.sort_unstable();
+            for i in nodes.into_iter() {
+                if workers.iter().any(|w| w.node == i) {
+                    continue;
+                }
+                if has_no_incoming(&g, i) {
+                    workers[idx] = Worker {
+                        node: i,
+                        time: elapsed + cost(i),
+                    };
+                    did_work = true;
+                    break;
+                }
+            }
+        }
+        if !did_work {
+            elapsed += 1;
+        }
+    }
+    elapsed - 1
+}
+
+#[derive(Debug, Clone, Copy)]
+struct Worker {
+    node: char,
+    time: i32,
+}
+
+fn has_no_incoming(g: &DiGraphMap<char, ()>, node: char) -> bool {
+    g.neighbors_directed(node, Direction::Incoming)
+        .next()
+        .is_none()
 }
 
 fn cost(node: char) -> i32 {
-    let c = node.to_ascii_uppercase() as u8 - 97;
-    i32::from(c)
+    let c = node.to_ascii_uppercase() as u8 - 64;
+    i32::from(c) + 60
 }
