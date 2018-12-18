5282e7c10e11ac368d43606a3e192610cc068847
CJ Carey <perimosocordiae@gmail.com>
2018-12-18 12:37:28 -0500

    Finally fixing day15

--- src/day15.rs
+++ src/day15.rs
@@ -4,20 +4,47 @@ use std::collections::HashSet;
 use std::fs;
 
 pub fn run() {
-    let (map, units) = setup("inputs/15-small6.txt");
+    let (map, units) = setup("inputs/15.txt");
     println!("Part 1: {}", part1(&map, &units));
-    //println!("Part 2: {}", part2(&map, &units));
+    println!("Part 2: {}", part2(&map, &units));
 }
 
 fn part1(grid: &[Vec<Cell>], initial_units: &[Unit]) -> i32 {
+    let (num_rounds, units) = run_battle(&grid, &initial_units, 3, false);
+    let total_hp: i32 = units.iter().filter(|u| u.hp > 0).map(|u| u.hp).sum();
+    num_rounds * total_hp
+}
+
+fn part2(grid: &[Vec<Cell>], initial_units: &[Unit]) -> i32 {
+    // TODO: binary search here?
+    for elf_attack in 4..100 {
+        let (num_rounds, units) = run_battle(&grid, &initial_units, elf_attack, false);
+        if units
+            .iter()
+            .filter(|u| u.team == Team::Elf)
+            .all(|u| u.hp > 0)
+        {
+            let total_hp: i32 = units.iter().filter(|u| u.hp > 0).map(|u| u.hp).sum();
+            return num_rounds * total_hp;
+        }
+    }
+    0
+}
+
+fn run_battle(
+    grid: &[Vec<Cell>],
+    initial_units: &[Unit],
+    elf_attack: i32,
+    debug: bool,
+) -> (i32, Vec<Unit>) {
     let num_units = initial_units.len();
     let mut units = initial_units.to_vec();
     let mut num_rounds = 0i32;
     while num_rounds < 99999 {
         units.sort_unstable_by_key(|u| (u.hp <= 0, u.row, u.col));
-        let scores: Vec<i32> = units.iter().map(|u| u.hp).collect();
-        println!("{}: {:?}", num_rounds, scores);
-        display_state(&grid, &units);
+        if debug {
+            display_state(&grid, &units);
+        }
         for idx in 0..num_units {
             let unit = &units[idx];
             if unit.hp <= 0 {
@@ -30,33 +57,29 @@ fn part1(grid: &[Vec<Cell>], initial_units: &[Unit]) -> i32 {
                 .map(|(i, _)| i)
                 .collect();
             if targets_idx.is_empty() {
-                let scores: Vec<i32> = units.iter().map(|u| u.hp).collect();
-                println!("Final: {:?}", scores);
-                display_state(&grid, &units);
-                return combat_end(num_rounds, &units);
+                return (num_rounds, units);
             }
+            let attack_power = match unit.team {
+                Team::Elf => elf_attack,
+                Team::Goblin => 3,
+            };
             if let Some(tidx) = try_attack(&units, idx, &targets_idx) {
-                //println!("Unit {} attacking {} without move", idx, tidx);
-                units[tidx].hp -= 3;
+                units[tidx].hp -= attack_power;
             } else if let Some((r, c)) = find_move(&grid, &units, idx, &targets_idx) {
                 // move to (r,c)
-                //println!("Unit {} moving", idx);
                 units[idx].row = r;
                 units[idx].col = c;
                 if let Some(tidx) = try_attack(&units, idx, &targets_idx) {
-                    //println!("Unit {} attacking {} after move", idx, tidx);
-                    units[tidx].hp -= 3;
+                    units[tidx].hp -= attack_power;
                 }
-            } else {
-                //println!("Unit {} cannot move!", idx);
             }
         }
         if num_units == 0 {
-            return 0;
+            return (num_rounds, units);
         }
         num_rounds += 1;
     }
-    0
+    (0, units)
 }
 
 fn is_open(row: i32, col: i32, grid: &[Vec<Cell>], units: &[Unit]) -> bool {
@@ -72,11 +95,6 @@ fn is_open(row: i32, col: i32, grid: &[Vec<Cell>], units: &[Unit]) -> bool {
 fn next_to(a: &Unit, b: &Unit) -> bool {
     let dist = (a.row - b.row).abs() + (a.col - b.col).abs();
     dist == 1
-}
-
-fn combat_end(num_rounds: i32, units: &[Unit]) -> i32 {
-    let total_hp: i32 = units.iter().filter(|u| u.hp >= 0).map(|u| u.hp).sum();
-    num_rounds * total_hp
 }
 
 fn setup(path: &str) -> (Vec<Vec<Cell>>, Vec<Unit>) {
@@ -149,31 +167,32 @@ fn display_state(grid: &[Vec<Cell>], units: &[Unit]) {
                 _ => '.',
             })
             .collect();
+        let mut pieces = vec![];
         for u in units.iter() {
             if u.hp > 0 && u.row == row as i32 {
-                c[u.col as usize] = match u.team {
+                let icon = match u.team {
                     Team::Elf => 'E',
                     _ => 'G',
                 };
+                c[u.col as usize] = icon;
+                pieces.push(format!("{}({})", icon, u.hp).to_string());
             }
         }
         let s: String = c.into_iter().collect();
-        println!("{}", s);
+        let t: String = pieces.join(", ");
+        println!("{}   {}", s, t);
     }
+    println!("");
 }
 
 fn try_attack(units: &[Unit], idx: usize, targets_idx: &[usize]) -> Option<usize> {
     let unit = &units[idx];
-    let in_range: Vec<usize> = targets_idx
+    // Targets were in sorted order originally, but may have moved since then.
+    targets_idx
         .iter()
         .filter(|&i| next_to(&unit, &units[*i]))
         .cloned()
-        .collect();
-    if !in_range.is_empty() {
-        let tidx = *in_range.iter().min_by_key(|&i| &units[*i].hp).unwrap();
-        return Some(tidx);
-    }
-    None
+        .min_by_key(|&i| (units[i].hp, units[i].row, units[i].col))
 }
 
 fn find_move(
@@ -193,41 +212,63 @@ fn find_move(
         return None;
     }
     let unit = &units[start];
-    let start_loc = (unit.row, unit.col);
-    let mut prev = HashMap::new();
+    let moves = next_moves(&grid, &units, unit.row, unit.col);
+    let mut best_move = None;
+    let mut best_key = (99999, (0, 0));
+    for m in moves {
+        if let Some(key) = shortest_path(&grid, &units, m, &target_locs) {
+            if best_key > key {
+                best_key = key;
+                best_move = Some(m);
+            }
+        }
+    }
+    best_move
+}
+
+fn shortest_path(
+    grid: &[Vec<Cell>],
+    units: &[Unit],
+    start_loc: (i32, i32),
+    target_locs: &HashSet<(i32, i32)>,
+) -> Option<(i32, (i32, i32))> {
     let mut dist = HashMap::new();
     dist.insert(start_loc, 0);
     let mut queue = BinaryHeap::new();
     queue.push((0, 0, start_loc));
+    let large_cost = 99999;
+    let mut max_cost = large_cost;
     while let Some((neg_cost, _, loc)) = queue.pop() {
-        //println!("loc: {:?} cost: {} queue: {:?}", loc, -neg_cost, queue);
+        let cost = -neg_cost;
+        if cost > *dist.get(&loc).unwrap_or(&large_cost) {
+            continue;
+        }
         if target_locs.contains(&loc) {
-            let mut p = loc;
-            loop {
-                let q = *prev.get(&p).unwrap();
-                if q == start_loc {
-                    return Some(p);
-                }
-                p = q;
+            if max_cost > cost {
+                max_cost = cost;
             }
         }
-        let cost = -neg_cost;
-        if cost > *dist.get(&loc).unwrap() {
+        if cost >= max_cost {
             continue;
         }
         let children = next_moves(&grid, &units, loc.0, loc.1);
-        //println!("children: {:?}", children);
         for (i, c) in children.into_iter().enumerate() {
             let c_cost = cost + 1;
-            let d = *dist.get(&c).unwrap_or(&99999);
+            let d = *dist.get(&c).unwrap_or(&large_cost);
             if c_cost < d {
                 queue.push((-c_cost, 4 - i, c));
                 dist.insert(c, c_cost);
-                prev.insert(c, loc);
             }
         }
     }
-    None
+    if max_cost == large_cost {
+        return None;
+    }
+    let target_loc = *target_locs
+        .iter()
+        .min_by_key(|loc| (*dist.get(&loc).unwrap_or(&large_cost), loc.clone()))
+        .expect("No target_locs");
+    Some((max_cost, target_loc))
 }
 
 fn next_moves(grid: &[Vec<Cell>], units: &[Unit], r: i32, c: i32) -> Vec<(i32, i32)> {
