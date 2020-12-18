e3deecb4d683aa21de145f346de2886c075094fc
CJ Carey <perimosocordiae@gmail.com>
2020-12-17 21:05:58 -0500

    Cleaning up day 17 a bit

--- advent2020/src/bin/day17.rs
+++ advent2020/src/bin/day17.rs
@@ -1,4 +1,5 @@
 use advent2020::*;
+use itertools::Itertools;
 use std::collections::HashMap;
 
 fn main() {
@@ -13,8 +14,12 @@ fn part1_small() {
     assert_eq!(part1(&grid), 112);
 }
 
-fn part1(grid: &[Point]) -> usize {
-    run_n_cycles(grid, true, 6).len()
+fn part1(initial_grid: &[Point]) -> usize {
+    let mut grid = initial_grid.to_vec();
+    for _ in 0..6 {
+        grid = run_step(&grid, 3);
+    }
+    grid.len()
 }
 
 #[test]
@@ -23,28 +28,16 @@ fn part2_small() {
     assert_eq!(part2(&grid), 848);
 }
 
-fn part2(grid: &[Point]) -> usize {
-    run_n_cycles(grid, false, 6).len()
-}
-
-fn run_n_cycles(
-    grid: &[Point],
-    is_part1: bool,
-    num_cycles: usize,
-) -> Vec<Point> {
-    if num_cycles == 0 {
-        grid.to_vec()
-    } else {
-        run_n_cycles(&run_step(grid, is_part1), is_part1, num_cycles - 1)
+fn part2(initial_grid: &[Point]) -> usize {
+    let mut grid = initial_grid.to_vec();
+    for _ in 0..6 {
+        grid = run_step(&grid, 4);
     }
+    grid.len()
 }
 
-fn run_step(grid: &[Point], is_part1: bool) -> Vec<Point> {
-    let neighbors = if is_part1 {
-        active_neighbors1(&grid)
-    } else {
-        active_neighbors2(&grid)
-    };
+fn run_step(grid: &[Point], dimension: usize) -> Vec<Point> {
+    let neighbors = active_neighbors(&grid, dimension);
     let mut next_grid = vec![];
     for p in grid {
         if let Some(n) = neighbors.get(&p) {
@@ -61,46 +54,16 @@ fn run_step(grid: &[Point], is_part1: bool) -> Vec<Point> {
     next_grid
 }
 
-fn active_neighbors1(grid: &[Point]) -> HashMap<Point, usize> {
+fn active_neighbors(grid: &[Point], dim: usize) -> HashMap<Point, usize> {
     let mut res = HashMap::new();
     for p in grid {
-        for dx in [-1, 0, 1].iter() {
-            for dy in [-1, 0, 1].iter() {
-                for dz in [-1, 0, 1].iter() {
-                    let neighbor = Point {
-                        x: p.x + dx,
-                        y: p.y + dy,
-                        z: p.z + dz,
-                        w: 0,
-                    };
-                    if neighbor != *p {
-                        *res.entry(neighbor).or_insert_with(|| 0) += 1;
-                    }
-                }
-            }
-        }
-    }
-    res
-}
-
-fn active_neighbors2(grid: &[Point]) -> HashMap<Point, usize> {
-    let mut res = HashMap::new();
-    for p in grid {
-        for dx in [-1, 0, 1].iter() {
-            for dy in [-1, 0, 1].iter() {
-                for dz in [-1, 0, 1].iter() {
-                    for dw in [-1, 0, 1].iter() {
-                        let neighbor = Point {
-                            x: p.x + dx,
-                            y: p.y + dy,
-                            z: p.z + dz,
-                            w: p.w + dw,
-                        };
-                        if neighbor != *p {
-                            *res.entry(neighbor).or_insert_with(|| 0) += 1;
-                        }
-                    }
-                }
+        for deltas in (0..dim)
+            .map(|_| [-1, 0, 1 as i64].iter())
+            .multi_cartesian_product()
+        {
+            let neighbor = p.offset(deltas);
+            if neighbor != *p {
+                *res.entry(neighbor).or_insert_with(|| 0) += 1;
             }
         }
     }
@@ -113,6 +76,21 @@ struct Point {
     y: i64,
     z: i64,
     w: i64,
+}
+impl Point {
+    fn offset(&self, deltas: Vec<&i64>) -> Self {
+        let w = if deltas.len() == 4 {
+            self.w + deltas[3]
+        } else {
+            0
+        };
+        Point {
+            x: self.x + deltas[0],
+            y: self.y + deltas[1],
+            z: self.z + deltas[2],
+            w,
+        }
+    }
 }
 
 fn parse_grid(path: &str) -> Vec<Point> {
