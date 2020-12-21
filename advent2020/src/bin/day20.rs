2b08c426255a993207e6d0e2bac4be71e176879b
CJ Carey <perimosocordiae@gmail.com>
2020-12-20 23:52:41 -0500

    Day 20 part 1

---
+++ advent2020/src/bin/day20.rs
@@ -0,0 +1,84 @@
+use advent2020::*;
+use itertools::Itertools;
+use std::collections::HashMap;
+use std::collections::HashSet;
+
+#[macro_use]
+extern crate scan_fmt;
+
+fn main() {
+    let tiles = parse_tiles("inputs/20.full");
+    println!("part1: {}", part1(&tiles));
+}
+
+#[test]
+fn part1_small() {
+    let tiles = parse_tiles("inputs/20.test");
+    assert_eq!(part1(&tiles), 20899048083289);
+}
+
+fn part1(tiles: &HashMap<usize, Tile>) -> usize {
+    let mut sketches = HashMap::<String, Vec<usize>>::new();
+    for (tile_id, tile) in tiles {
+        let sides = vec![&tile.top, &tile.left, &tile.right, &tile.bottom];
+        for side in sides {
+            let rev_side = side.chars().rev().collect::<String>();
+            sketches
+                .entry(side.to_string())
+                .or_insert_with(Vec::new)
+                .push(*tile_id);
+            sketches
+                .entry(rev_side)
+                .or_insert_with(Vec::new)
+                .push(*tile_id);
+        }
+    }
+    let mut graph = HashMap::<usize, HashSet<usize>>::new();
+    for bucket in sketches.values() {
+        for pair in bucket.iter().combinations(2) {
+            let a = pair[0];
+            let b = pair[1];
+            graph.entry(*a).or_insert_with(HashSet::new).insert(*b);
+            graph.entry(*b).or_insert_with(HashSet::new).insert(*a);
+        }
+    }
+    graph
+        .iter()
+        .filter(|(_, neighbors)| neighbors.len() == 2)
+        .map(|(tile_id, _)| tile_id)
+        .product()
+}
+
+#[derive(Debug)]
+struct Tile {
+    top: String,
+    left: String,
+    right: String,
+    bottom: String,
+}
+
+fn parse_tiles(path: &str) -> HashMap<usize, Tile> {
+    let data = read_string(path);
+    let mut out = HashMap::new();
+    for chunk in data.split("\n\n") {
+        let tile_id = scan_fmt!(&chunk, "Tile {d}:", usize).unwrap();
+        let tile_lines: Vec<&str> = chunk.lines().skip(1).collect();
+        let n = tile_lines.len();
+        let left: String = (0..n)
+            .map(|i| tile_lines[i].chars().next().unwrap())
+            .collect();
+        let right: String = (0..n)
+            .map(|i| tile_lines[i].chars().last().unwrap())
+            .collect();
+        out.insert(
+            tile_id,
+            Tile {
+                top: tile_lines[0].to_string(),
+                bottom: tile_lines[n - 1].to_string(),
+                left,
+                right,
+            },
+        );
+    }
+    out
+}
