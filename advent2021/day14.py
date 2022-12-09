455c08ff3c733a24efe703de9aba68f2efe09a7c
CJ Carey <perimosocordiae@gmail.com>
2022-12-08 20:31:16 -0500

    2021 day 14 part 2
    Leaving the slow version in place for posterity

--- advent2021/day14.py
+++ advent2021/day14.py
@@ -15,18 +15,42 @@ def parse_input(path: str) -> tuple[str, dict[str, str]]:
 def solve(path: str) -> None:
     print(path)
     polymer, rules = parse_input(path)
-    for _ in range(1, 11):
+    print("Part 1:", part1(polymer, rules))
+    print("Part 2:", part2(polymer, rules))
+    print()
+
+
+def part1(polymer: str, rules: dict[str, str]) -> int:
+    for _ in range(10):
         result = polymer[0]
-        for i in range(1, len(polymer)):
-            pair = polymer[i - 1 : i + 1]
+        for i in range(len(polymer) - 1):
+            pair = polymer[i : i + 2]
             to_insert = rules.get(pair, "")
             result += to_insert + pair[1]
         polymer = result
     counts = Counter(polymer).most_common()
-    print("Part 1:", counts[0][1] - counts[-1][1])
-    # TODO: Find the relationship and avoid growing the polymer
-    print("Part 2:", "...")
-    print()
+    return counts[0][1] - counts[-1][1]
+
+
+def part2(polymer: str, rules: dict[str, str]) -> int:
+    pairs = Counter(polymer[i : i + 2] for i in range(len(polymer) - 1))
+    for _ in range(40):
+        new_pairs = Counter()
+        for pair, count in pairs.items():
+            if pair in rules:
+                ch = rules[pair]
+                new_pairs[pair[0] + ch] += count
+                new_pairs[ch + pair[1]] += count
+            else:
+                new_pairs[pair] += count
+        pairs = new_pairs
+    singles = Counter()
+    for pair, count in pairs.items():
+        if not singles:
+            singles[pair[0]] += count
+        singles[pair[1]] += count
+    counts = singles.most_common()
+    return counts[0][1] - counts[-1][1]
 
 
 if __name__ == "__main__":
