cd779fac311d54947bcd3bc7f6b1be2b6d5bc2f4
CJ Carey <perimosocordiae@gmail.com>
2022-12-13 20:04:02 -0500

    2022 day 13 part 2

--- advent2022/day13.py
+++ advent2022/day13.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python3
 import ast
-import itertools
 from functools import cmp_to_key
 
 
@@ -11,14 +10,10 @@ def compare(left, right) -> int:
         return compare([left], right)
     if isinstance(right, int):
         return compare(left, [right])
-    for lx, rx in itertools.zip_longest(left, right):
-        if lx is None:
-            return -1
-        if rx is None:
-            return 1
-        if lx != rx:
-            return compare(lx, rx)
-    return 0
+    for lx, rx in zip(left, right):
+        if res := compare(lx, rx):
+            return res
+    return len(left) - len(right)
 
 
 def solve(path: str) -> None:
@@ -37,11 +32,9 @@ def solve(path: str) -> None:
     ]
     packets.extend([divider1, divider2])
     packets.sort(key=cmp_to_key(compare))
-    print(*packets, sep="\n")
     idx1 = packets.index(divider1) + 1
     idx2 = packets.index(divider2) + 1
     print("Part 2:", idx1 * idx2)
-    assert idx1 * idx2 < 28359
 
 
 if __name__ == "__main__":
