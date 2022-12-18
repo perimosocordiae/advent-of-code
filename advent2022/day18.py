ec0409ae68c3bfc5a7df3b29562d35ae79f8c7c9
CJ Carey <perimosocordiae@gmail.com>
2022-12-18 17:08:50 -0500

    2022 day 18 part 2
    Thank you scipy :-)

--- advent2022/day18.py
+++ advent2022/day18.py
@@ -1,12 +1,29 @@
 #!/usr/bin/env python3
 import numpy as np
+import scipy.ndimage as ndi
+
+
+def surface_area(coords) -> int:
+    dist = np.abs(coords[:, None] - coords).sum(axis=2)
+    return (6 - (dist == 1).sum(axis=0)).sum()
 
 
 def solve(path: str) -> None:
     coords = np.loadtxt(path, dtype=int, delimiter=",")
-    dist = np.abs(coords[:, None] - coords).sum(axis=2)
-    surface_area = (6 - (dist == 1).sum(axis=0)).sum()
-    print("Part 1:", surface_area)
+    total_surface = surface_area(coords)
+    print("Part 1:", total_surface)
+
+    min_pt = coords.min(axis=0)
+    max_pt = coords.max(axis=0)
+    shape = max_pt - min_pt + 1
+    grid = np.ones(shape, dtype=int)
+    i, j, k = (coords - min_pt).T
+    grid[i, j, k] = 0
+    labels, n = ndi.label(grid)
+    external_surface = total_surface
+    for i in range(2, n + 1):
+        external_surface -= surface_area(np.column_stack(np.where(labels == i)))
+    print("Part 2:", external_surface)
 
 
 if __name__ == "__main__":
