ca6e539fb312df7111953f85830769b57450d71d
CJ Carey <perimosocordiae@gmail.com>
2022-12-17 21:51:41 -0500

    2022 day 16 part 2
    This takes about an hour to run on my old laptop :(

--- advent2022/day16.py
+++ advent2022/day16.py
@@ -17,7 +17,7 @@ class Valve:
         return name, Valve(rate, tunnels)
 
 
-def solve(path: str) -> None:
+def parse_input(path: str) -> tuple[np.ndarray, np.ndarray]:
     valves = dict(Valve.from_str(line) for line in open(path))
     nodes = sorted(valves.keys())
     n = len(nodes)
@@ -39,7 +39,11 @@ def solve(path: str) -> None:
     adj = adj[inds, inds[:, None]]
     rates = rates[inds]
     rates[0] = 0
+    return adj, rates
 
+
+def part1(path: str) -> None:
+    adj, rates = parse_input(path)
     max_flow = 0
     # Stores (flow, node, time, rates)
     queue = [(0, 0, 30, rates)]  # type: list[tuple[int, int, int, np.ndarray]]
@@ -60,5 +64,70 @@ def solve(path: str) -> None:
     print("Part 1:", max_flow)
 
 
+def part2(path: str) -> None:
+    adj, rates = parse_input(path)
+    max_flow = 0
+    # Stores (flow, node1, node2, time, rates)
+    queue = [
+        (0, (0, 26), (0, 26), rates)
+    ]  # type: list[tuple[int, tuple[int, int], tuple[int, int], np.ndarray]]
+    while queue:
+        flow, (n1, t1), (n2, t2), rates = queue.pop()
+        if t1 >= t2 and rates[n1] > 0:
+            new_flow = flow + rates[n1] * (t1 - 1)
+            new_rates = rates.copy()
+            new_rates[n1] = 0
+            queue.append((new_flow, (n1, t1 - 1), (n2, t2), new_rates))
+            if new_flow > max_flow:
+                max_flow = new_flow
+                print(new_flow)
+            continue
+        if t2 > t1 and rates[n2] > 0:
+            new_flow = flow + rates[n2] * (t2 - 1)
+            new_rates = rates.copy()
+            new_rates[n2] = 0
+            queue.append((new_flow, (n1, t1), (n2, t2 - 1), new_rates))
+            if new_flow > max_flow:
+                max_flow = new_flow
+                print(new_flow)
+            continue
+        if t1 > t2 and rates[n1] == 0:
+            for nn1 in np.nonzero(rates)[0]:
+                cost = adj[n1, nn1]
+                if t1 > cost:
+                    queue.append((flow, (nn1, t1 - cost), (n2, t2), rates))
+            continue
+        if t2 > t1 and rates[n2] == 0:
+            for nn2 in np.nonzero(rates)[0]:
+                cost = adj[n2, nn2]
+                if t2 > cost:
+                    queue.append((flow, (n1, t1), (nn2, t2 - cost), rates))
+            continue
+        if t1 == t2:
+            if rates[n1] == 0 and rates[n2] == 0:
+                (inds,) = np.nonzero(rates)
+                for nn1 in inds:
+                    cost1 = adj[n1, nn1]
+                    if t1 > cost1:
+                        for nn2 in inds:
+                            cost2 = adj[n2, nn2]
+                            if nn2 != nn1 and t2 > cost2:
+                                queue.append(
+                                    (flow, (nn1, t1 - cost1), (nn2, t2 - cost2), rates)
+                                )
+            elif rates[n1] == 0:
+                for nn1 in np.nonzero(rates)[0]:
+                    cost1 = adj[n1, nn1]
+                    if t1 > cost1:
+                        queue.append((flow, (nn1, t1 - cost1), (n2, t2), rates))
+            elif rates[n2] == 0:
+                for nn2 in np.nonzero(rates)[0]:
+                    cost2 = adj[n2, nn2]
+                    if t2 > cost2:
+                        queue.append((flow, (n1, t1), (nn2, t2 - cost2), rates))
+    print("Part 2:", max_flow)
+
+
 if __name__ == "__main__":
-    solve("inputs/16.full")
+    part1("inputs/16.full")
+    part2("inputs/16.full")
