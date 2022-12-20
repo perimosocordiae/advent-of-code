458e336dfd082773a754b9f3708b0ccd492886db
CJ Carey <perimosocordiae@gmail.com>
2022-12-20 00:54:39 -0500

    2022 day 19, both parts

--- advent2022/day19.py
+++ advent2022/day19.py
@@ -1,107 +1,72 @@
 #!/usr/bin/env python3
 import numpy as np
+import numpy.typing as npt
+
+Materials = npt.NDArray[np.int_]
 
 
-class Materials:
-    def __init__(self, ore=0, clay=0, obsidian=0, geode=0) -> None:
-        self.x = np.array([ore, clay, obsidian, geode], dtype=int)
-
-    @property
-    def geode(self) -> int:
-        return self.x[3]
-
-    def all_satisfied(self, other: "Materials") -> bool:
-        return bool((self.x >= other.x).all())
-
-    def sort_key(self) -> int:
-        return np.inner(self.x, [1, 100, 10000, 1000000])
-
-    def __add__(self, other: "Materials") -> "Materials":
-        return Materials(*self.x + other.x)
-
-    def __sub__(self, other: "Materials") -> "Materials":
-        return Materials(*self.x - other.x)
-
-    def __repr__(self) -> str:
-        return repr(self.x)
-
-    def __str__(self) -> str:
-        ore, clay, obsidian, geode = self.x
-        res = []
-        if ore > 0:
-            res.append(f"{ore} ore")
-        if clay > 0:
-            res.append(f"{clay} clay")
-        if obsidian > 0:
-            res.append(f"{obsidian} obsidian")
-        if geode > 0:
-            res.append(f"{geode} geode")
-        return ", ".join(res)
-
-    def __hash__(self) -> int:
-        return hash(tuple(self.x))
+def mats(ore=0, clay=0, obsidian=0) -> Materials:
+    return np.array([ore, clay, obsidian], dtype=int)
 
 
-def max_geodes_old(blueprint: dict[str, Materials]) -> int:
-    """Finds the max number of geodes that can be produced in 24 minutes."""
-    best_geodes = 0
-    queue = [
-        (24, Materials(ore=1), Materials())
-    ]  # type: list[tuple[int, Materials, Materials]]
-    while queue:
-        time, robots, inventory = queue.pop()
-        if time == 0:
-            if inventory.geode > best_geodes:
-                best_geodes = inventory.geode
-                print("New best:", best_geodes)
-            continue
-        for kind in ["ore", "clay", "obsidian", "geode"]:
-            cost = blueprint[kind]
-            if inventory.all_satisfied(cost):
-                new_inventory = inventory - cost + robots
-                new_robots = robots + Materials(**{kind: 1})
-                queue.append((time - 1, new_robots, new_inventory))
-        inventory += robots
-        queue.append((time - 1, robots, inventory))
-        queue.sort(key=lambda x: x[1].sort_key())
-    return best_geodes
+def max_geodes(costs: Materials, yields: Materials, total_time: int) -> int:
+    """Finds the max number of geodes that can be produced in T minutes."""
+    max_cost = costs.max(axis=0)
 
-
-def max_geodes(blueprint: dict[str, Materials]) -> int:
-    """Finds the max number of geodes that can be produced in 24 minutes."""
-
-    def rec(time: int, robots: Materials, inventory: Materials) -> int:
-        if time == 1:
-            return inventory.geode + robots.geode
-        best = rec(time - 1, robots, inventory + robots)
-        for kind in ["ore", "clay", "obsidian", "geode"]:
-            cost = blueprint[kind]
-            if inventory.all_satisfied(cost):
-                new_inventory = inventory - cost + robots
-                new_robots = robots + Materials(**{kind: 1})
-                best = max(best, rec(time - 1, new_robots, new_inventory))
+    def rec(time: int, robots: Materials, inventory: Materials, last: int) -> int:
+        if time <= 1:
+            return 0
+        maxd = max_cost * time - inventory
+        need = robots * time <= maxd
+        build = (inventory >= costs).all(axis=1)
+        build[:3] &= robots < max_cost
+        last_build = (inventory - robots >= costs).all(axis=1)
+        t = time - 1
+        inv = inventory + robots  # type: Materials
+        if build[3]:
+            return rec(t, robots, inv - costs[3], 3) + t
+        best = 0
+        if build[2] and need[2] and not (last in (2, -1) and last_build[2]):
+            best = max(best, rec(t, robots + yields[2], inv - costs[2], 2))
+        if build[1] and need[1] and need[2] and not (last in (1, -1) and last_build[1]):
+            best = max(best, rec(t, robots + yields[1], inv - costs[1], 1))
+        if build[0] and need[0] and not (last in (0, -1) and last_build[0]):
+            best = max(best, rec(t, robots + yields[0], inv - costs[0], 0))
+        if not build[0]:
+            best = max(best, rec(t, robots, inv, -1))
         return best
 
-    return rec(24, Materials(ore=1), Materials())
+    return rec(total_time, mats(ore=1), mats(), -1)
 
 
 def solve(path: str) -> None:
-    sum_geodes = 0
+    blueprints = []
     for line in open(path):
         parts = line.split()
         if len(parts) < 31:
             continue
         num = int(parts[1].rstrip(":"))
-        blueprint = {
-            "ore": Materials(ore=int(parts[6])),
-            "clay": Materials(ore=int(parts[12])),
-            "obsidian": Materials(ore=int(parts[18]), clay=int(parts[21])),
-            "geode": Materials(ore=int(parts[27]), obsidian=int(parts[30])),
-        }
-        print(num, blueprint)
-        sum_geodes += max_geodes(blueprint)
-    print("Part 1:", sum_geodes)
+        costs = np.array(
+            [
+                mats(ore=int(parts[6])),
+                mats(ore=int(parts[12])),
+                mats(ore=int(parts[18]), clay=int(parts[21])),
+                mats(ore=int(parts[27]), obsidian=int(parts[30])),
+            ]
+        )
+        blueprints.append((num, costs))
+
+    yields = np.eye(3, dtype=int)
+    sum_quality = 0
+    for num, costs in blueprints:
+        sum_quality += num * max_geodes(costs, yields, 24)
+    print("Part 1:", sum_quality)
+
+    prod_geodes = 1
+    for _, costs in blueprints[:3]:
+        prod_geodes *= max_geodes(costs, yields, 32)
+    print("Part 2:", prod_geodes)
 
 
 if __name__ == "__main__":
-    solve("inputs/19.test")
+    solve("inputs/19.full")
