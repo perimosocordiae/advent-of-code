e6fc3c4fe7858a2882cb97e863322b2a8e192800
CJ Carey <perimosocordiae@gmail.com>
2024-12-04 00:09:15 -0500

    Add comments to shell pipelines

--- advent2024/day01.sh
+++ advent2024/day01.sh
@@ -7,13 +7,18 @@ set -euo pipefail
 infile=${1:-'inputs/01.full'}
 
 echo "Part 1:"
+# Sort the columns separately, then paste them together with a '-' separator
 paste -d'-' \
  <(cut -f1 -d' ' "$infile" | sort -n) \
- <(cut -f4 -d' ' "$infile" | sort -n) \
-| bc | sed 's/-//' | paste -s -d+ - | bc
+ <(cut -f4 -d' ' "$infile" | sort -n) |
+ # Compute the absolute differences
+ bc | sed 's/-//' |
+ # Sum the results
+ paste -s -d+ - | bc
 
 echo
 echo "Part 2:"
+# Count the frequency of each number in the input
 awk '{ x[$1]++; y[$2]++; }
 END {
  for (i in x) { sum += x[i] * y[i] * i; }
--- advent2024/day03.sh
+++ advent2024/day03.sh
@@ -7,14 +7,24 @@ set -euo pipefail
 infile=${1:-'inputs/03.full'}
 
 echo "Part 1:"
-grep -o 'mul([0-9]\+,[0-9]\+)' "$infile" \
- | tr ',mul' '*   ' | paste -s -d+ | bc
+# Select all valid mul(a,b) expressions
+grep -o 'mul([0-9]\+,[0-9]\+)' "$infile" |
+ # Replace mul(a,b) with (a*b)
+ tr ',mul' '*   ' |
+ # Sum all the products
+ paste -s -d+ | bc
 
 echo
 echo "Part 2:"
-grep -E -o "(mul|do|don't)\(([0-9]+,[0-9]+)?\)" "$infile" \
-| tr ',mul' '*   ' | awk '
+# Select all mul(a,b), do(), and don't() expressions
+grep -E -o "(mul|do|don't)\(([0-9]+,[0-9]+)?\)" "$infile" |
+ # Replace mul(a,b) with (a*b)
+ tr ',mul' '*   ' |
+ # Drop lines suppressed by don't()
+ awk '
 BEGIN { on=1; }
 /^don/ { on=0; }
 /^do\(/ { on=1; }
-/^ / { if (on) { print $0; } }' | paste -s -d+ | bc
+/^ / { if (on) { print $0; } }' |
+ # Sum all the remaining products
+ paste -s -d+ | bc
