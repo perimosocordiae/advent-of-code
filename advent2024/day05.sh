aa16a26adc6df742bb8ff02da68061d2fb2fc26b
CJ Carey <perimosocordiae@gmail.com>
2024-12-07 00:28:11 -0500

    Day 5 part 2 in bash

--- advent2024/day05.sh
+++ advent2024/day05.sh
@@ -6,14 +6,39 @@ set -euo pipefail
 
 infile=${1:-'inputs/05.full'}
 
+# Extract edge information to a temporary file.
 grep '|' "$infile" | tr '|' ' ' >order.tmp
+
 echo "Part 1:"
+# Process lists of nodes, one per line.
 grep ',' "$infile" | tr ',' '|' | while read -r line; do
+  # Replace each node with its index in the topological sort.
   replacements=$(grep -E "($line) ($line)" order.tmp |
     tsort | nl -v0 |
     awk '{print "s/\\b"$2"\\b/_"$1"/g"}' | sort | paste -sd';')
+  # Check if the tsort order is valid (i.e., in sorted order).
   if echo "$line" | sed "$replacements" | tr '|_' '\n ' | sort -nuC ; then
+    # If so, print the middle node.
     echo "$line" | awk 'BEGIN{FS="|";} { print $(NF/2+1); }'
   fi
 done | paste -sd+ | bc
+
+echo "Part 2:"
+# Process lists of nodes, one per line.
+grep ',' "$infile" | tr ',' '|' | while read -r line; do
+  # Replace each node with its index in the topological sort.
+  replacements=$(grep -E "($line) ($line)" order.tmp |
+    tsort | nl -v0 |
+    awk '{print "s/\\b"$2"\\b/_"$1"/g"}' | sort | paste -sd';')
+  # Check if the tsort order is invalid (i.e., not in sorted order).
+  if ! echo "$line" | sed "$replacements" | tr '|_' '\n ' | sort -nuC ; then
+    # Determine the 1-based index of the middle node in the sequence.
+    pipes="${line//[!|]/}"
+    np="${#pipes}"
+    mid=$(((np+3)/2))
+    # Select the middle node from the sort order.
+    grep -E "($line) ($line)" order.tmp | tsort | sed -n "${mid}p"
+  fi
+done | paste -sd+ | bc
+
 rm order.tmp
