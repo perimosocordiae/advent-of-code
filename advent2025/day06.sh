e0d10426384568758171831f7fce9db87d0bc1d6
CJ Carey <perimosocordiae@gmail.com>
2025-12-11 23:46:45 -0500

    2025 day 6 part 2 in awk

--- advent2025/day06.sh
+++ advent2025/day06.sh
@@ -6,6 +6,7 @@ set -euo pipefail
 
 infile=${1:-'inputs/06.full'}
 
+echo "Part 1:"
 awk '{
   for (i=1; i<=NF; i++) {
     print i, $i;
@@ -31,3 +32,42 @@ END {
   print res;
 }
 ' | paste -sd+ - | bc
+
+echo
+echo "Part 2:"
+awk '{
+  n = length($0);
+  for (i=1; i<=n; i++) {
+    print i, NR, substr($0, i, 1);
+  }
+}' "$infile" \
+ | sort -n \
+ | awk '
+{ lines[$1] = lines[$1] $3; }
+END {
+  for (i in lines) {
+    print i, lines[i];
+  }
+}' \
+ | sort -n \
+ | awk '
+BEGIN { op = ""; total = 0; num = 0; }
+$2 ~ /[0-9]+[*+]/ {
+  op = substr($2, length($2), 1);
+  num = substr($2, 1, length($2)-1);
+  next
+}
+$2 ~ /[0-9]+/ {
+  if (op == "+") {
+    num += $2;
+  } else if (op == "*") {
+    num *= $2;
+  } else {
+    print "Unknown operator " op > "/dev/stderr";
+    exit 1;
+  }
+  next
+}
+{ total += num; num = 0; op = ""; }
+END { print total + num; }
+'
